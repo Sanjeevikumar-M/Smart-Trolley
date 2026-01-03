b49691369ad6b47fc67424c502f0930232125a69
zoro1324 <naveen13524g@gmail.com>
2026-01-03 14:46:51 +0530

    Remove sessions and trolleys apps along with their models, views, serializers, and migrations; delete API endpoints and related configurations for a cleaner project structure.

--- App/server/sessions/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-
-
-class SessionsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'sessions'
-    label = 'trolley_sessions'  # Custom label to avoid conflict with django.contrib.sessions
-    verbose_name = 'Trolley Sessions'
--- App/server/trolleys/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class TrolleysConfig(AppConfig):
-    name = 'trolleys'
